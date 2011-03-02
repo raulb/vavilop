2dac2e5648348f2c9f7a597f1801a9f45010f1e4
rabarroso <ra.barroso@gmail.com>
2011-03-02 21:19:38 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vavilop::Application.config.secret_token = '5f570752519712ef2445ffd74e2d45a8053322517aaef362b9bc058b82b1467b1d9b5ea439f647ce869def9291d333b993c2e1baeaa974847248a502cb3fd49d'
